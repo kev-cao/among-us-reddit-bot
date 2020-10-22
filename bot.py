76ff48890e7333d988bf652f16d77c38374e7bca
Kevin Cao <kcao1998@gmail.com>
2020-10-22 15:32:48 -0400

    switching to psaw for more live data

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
-import praw, creds, settings, search, respond, json
+import praw, creds, settings, search, respond, json, time, traceback
+from psaw import PushshiftAPI
 
 # Connect bot to reddit and create reddit instance
 reddit_client = praw.Reddit(client_id = creds.client_id,
@@ -6,18 +7,23 @@ reddit_client = praw.Reddit(client_id = creds.client_id,
         refresh_token = creds.refresh_token,
         user_agent = settings.user_agent)
 
+# Use pushshift_api to retrieve comments
+pushshift_api = PushshiftAPI(reddit_client)
+
 # Get blacklist information.
 with open('blacklist.json') as blacklist_file:
     blacklist = json.load(blacklist_file)
 
 
+# Get starting time to filter only comments made since the bot launched.
+search_time = int(time.time())
+
 while True:
     try:
-        for comment in reddit_client.subreddit('all').stream.comments(skip_existing = True):
-            if comment.subreddit.name not in blacklist['disallowed']:
+        for comment in pushshift_api.search_comments(limit = 500, after = search_time):
+            if comment.subreddit.name.lower() not in blacklist['disallowed']:
                 username = search.check_trigger(comment.body)
                 if username:
                     comment.reply(respond.build_reply(username))
     except Exception as e:
         traceback.print_exc()
-
